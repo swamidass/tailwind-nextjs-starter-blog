632edca64c6ad374171d3e2e6e294af3b9e29ae5
S. Joshua Swamidass <swamidass@gmail.com>
2022-03-01 14:43:27 -0600

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.green,
         gray: colors.neutral,
       },
       typography: (theme) => ({
